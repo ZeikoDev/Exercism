e2871ccc0db038fffa3a2a65b45468684897f66f
Camilo G <jcgonzalez4183@gmail.com>
2023-10-07 01:03:08 -0500

    lasagna iteration 2

---
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -0,0 +1,60 @@
+"""Functions used in preparing Guido's gorgeous lasagna.
+
+Learn about Guido, the creator of the Python language:
+https://en.wikipedia.org/wiki/Guido_van_Rossum
+
+This is a module docstring, used to describe the functionality
+of a module and its functions and/or classes.
+"""
+
+
+
+EXPECTED_BAKE_TIME = 40 
+
+
+def bake_time_remaining(elapsed_bake_time):
+    
+    """Calculate the bake time remaining.
+
+    :param elapsed_bake_time: int - baking time already elapsed.
+    :return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+
+    
+    result = EXPECTED_BAKE_TIME - elapsed_bake_time
+    return result 
+
+
+def preparation_time_in_minutes(number_of_layers):
+    
+    """Calculate the preparation time in minutes.
+
+    :param number of layers: he number of layers you want to add to the lasagna.
+    :return: int * 2 (in minutes) .
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+    result = number_of_layers * 2
+    return result
+
+
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+     
+    """Calculate the elapsed time in minutes.
+
+    :param number_of_layers: (the number of layers added to the lasagna).
+    :Param elapsed_bake_time: (the number of minutes the lasagna has been baking in the oven)
+    :return: int + elapsed bake time (in minutes) .
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+    result = preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
+    return result
